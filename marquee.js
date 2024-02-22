d92e4b3775208507fba0d0de40d03e7c18722a52
Digital Rupture <69215896+VeteranWilds@users.noreply.github.com>
2024-02-22 17:12:39 -0500

    uodate worker domain

--- marquee.js
+++ marquee.js
@@ -2,7 +2,7 @@ const callsign = "ne5gg";
 const marqueeElement = document.querySelector(".marquee");
 
 async function fetchReport() {
-  const url = `https://wsprreport.ne5gg.workers.dev/`; // Replace with your Worker route
+  const url = `https://wsprreport.ne5gg.net/`; // Replace with your Worker route
   const response = await fetch(url);
 
   if (!response.ok) {
