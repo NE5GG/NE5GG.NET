da66766afa1b12b89899cc5107c0c8047f712f4e
Digital Rupture <69215896+VeteranWilds@users.noreply.github.com>
2024-02-22 16:52:37 -0500

    updated the worker request interval to 5000s

--- marquee.js
+++ marquee.js
@@ -2,7 +2,7 @@ const callsign = "ne5gg";
 const marqueeElement = document.querySelector(".marquee");
 
 async function fetchReport() {
-  const url = `https://your-worker-url/`; // Replace with your Worker route
+  const url = `https://wsprreport.ne5gg.workers.dev/`; // Replace with your Worker route
   const response = await fetch(url);
 
   if (!response.ok) {
@@ -20,4 +20,4 @@ async function fetchReport() {
 }
 
 fetchReport();
-setInterval(fetchReport, 5000); // Update every 5 seconds (adjust as needed)
+setInterval(fetchReport, 5000000); // Update every 5 seconds (adjust as needed)
