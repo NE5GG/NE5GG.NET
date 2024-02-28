caa1e434ccab7f64e6b2cfa72ed0c8bb2ecc3009
Digital Rupture <69215896+VeteranWilds@users.noreply.github.com>
2024-02-27 19:02:06 -0500

    update google onetap for fedCM

--- assets/js/google.js
+++ assets/js/google.js
@@ -1,25 +1,21 @@
-function initiateOneTap() {
-  // Configure Google One Tap options
-  const options = {
-    client_id: "203363874589-dtjqndbokl6kq0ekq1q4fssa5vn6sphe.apps.googleusercontent.com",
-    callback: function(response) {
-      // Handle the One Tap response
-      const profile = response.getBasicProfile();
-      const imageUrl = profile.getImageUrl();
+const options = {
+  client_id: "203363874589-dtjqndbokl6kq0ekq1q4fssa5vn6sphe.apps.googleusercontent.com",
+  callback: function(response) {
+    // Handle the One Tap response
+    const profile = response.getBasicProfile();
+    const imageUrl = profile.getImageUrl();
 
-      // Create a new img tag
-      const img = document.createElement('img');
-      img.src = imageUrl;
-      img.className = 'account-icon';
-      img.id = 'account_id';
+    // Create a new img tag
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.className = 'account-icon';
+    img.id = 'account_id';
 
-      // Replace the i tag with the img tag
-      const icon = document.getElementById('account_id');
-      icon.parentNode.replaceChild(img, icon);
-
-    },
-    prompt_parent_id: 'main_content', // The ID of the HTML element where the One Tap prompt will be displayed
-  };
+    // Replace the i tag with the img tag
+    const icon = document.getElementById('account_id');
+    icon.parentNode.replaceChild(img, icon);
+  },
+};
 
   // Initialize Google One Tap
   window.google.accounts.id.initialize(options);
