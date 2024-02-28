2f5b460576bf7aaa27309486117e2c6ee4d481b1
Digital Rupture <69215896+VeteranWilds@users.noreply.github.com>
2024-02-27 19:57:11 -0500

    update

--- assets/js/google.js
+++ assets/js/google.js
@@ -2,20 +2,16 @@ function handleCredentialResponse(response) {
   // Log the ID token
   console.log("ID: " + response.credential);
 
-  // Get the user's profile information
-  var profile = google.accounts.id.getBasicProfile();
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
+  // Get the user's full name and email
+  const fullName = response.name;
+  const email = response.email;
 
-  // Update the profile picture
-  const imageUrl = profile.getImageUrl();
-  const img = document.createElement('img');
-  img.src = imageUrl;
-  img.className = 'account-icon';
-  img.id = 'account_id';
-  const icon = document.getElementById('account_id');
-  icon.parentNode.replaceChild(img, icon);
+  // Split the full name into first and last name
+  const nameParts = fullName.split(' ');
+  const firstName = nameParts[0];
+  const lastName = nameParts.length > 1 ? nameParts[nameParts.length - 1] : '';
+
+  console.log('First Name: ' + firstName);
+  console.log('Last Name: ' + lastName);
+  console.log('Email: ' + email);
 }
