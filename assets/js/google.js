c7ebc56b95778027e0220bef62c3a8cbf1177b1a
Digital Rupture <69215896+VeteranWilds@users.noreply.github.com>
2024-02-27 20:05:24 -0500

    update google.js

--- assets/js/google.js
+++ assets/js/google.js
@@ -1,21 +1,34 @@
+function decodeJwt(jwt) {
+  // Split the JWT into parts
+  const parts = jwt.split('.');
+
+  // Check if the JWT includes the correct number of parts
+  if (parts.length !== 3) {
+    throw new Error('The JWT is not correctly formatted');
+  }
+
+  // Decode the payload
+  const payload = parts[1];
+  const decodedPayload = JSON.parse(atob(payload));
+
+  return decodedPayload;
+}
+
 function handleCredentialResponse(response) {
   // Log the ID token
   console.log("ID: " + response.credential);
 
-  // Get the user's full name and email
-  const fullName = response.name;
-  const email = response.email;
+  // Decode the JWT
+  const decodedJwt = decodeJwt(response.credential);
 
-  // Check if fullName is not undefined
-  if (fullName) {
-    // Split the full name into first and last name
-    const nameParts = fullName.split(' ');
-    const firstName = nameParts[0];
-    const lastName = nameParts.length > 1 ? nameParts[nameParts.length - 1] : '';
+  // Get the user's name and email
+  const firstName = decodedJwt.given_name;
+  const lastName = decodedJwt.family_name;
+  const email = decodedJwt.email;
+  const picture = decodedJwt.picture;
 
-    console.log('First Name: ' + firstName);
-    console.log('Last Name: ' + lastName);
-  }
-
+  console.log('First Name: ' + firstName);
+  console.log('Last Name: ' + lastName);
   console.log('Email: ' + email);
+  console.log('Picture: ' + picture);
 }
